927ea1774c5ffd3eae146a9ca2ced34c9a312e75
Darius Lukauskas <darius.lukauskas@staticus.lt>
2022-09-15 20:35:19 +0300

    Pridėtas žemės paviršiaus ploto ir žemės tūrio skaičiavimo algoritmas. Sukurti kintamieji reikalingi skaičiavimui atlikti.

--- Praktinis_darbas01/Program.cs
+++ Praktinis_darbas01/Program.cs
@@ -1,9 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-double izambine; //Vienas is kintamuju
+﻿double izambine; //Vienas is kintamuju
 
 
 izambine = Math.Sqrt(10 * 10 + 15 * 15); // Kintamojo reiksme priskirta
 
 
 Console.WriteLine(izambine); // Tiesiog pasitikrinau
+
+/*PARAŠYTI PROGRAMĄ KURI PRAŠO ĮVESTI ŽEMĖS RUTULIO SPINDULĮ, O IŠVEDA PAVIRŠIAUS ŽEMĖS PAVIRŠIAUS PLOTĄ IR TŪRĮ*/
+
+double sphereDiameter = 6371;
+double sphereRadius = sphereDiameter / 2;
+double numberPI = (double)Math.PI;
+
+double sphereSurfaceArea = 4 * numberPI * Math.Pow(sphereRadius, 2);
+double sphereVolume = 4.0 / 3 * numberPI * Math.Pow(sphereRadius, 3);
+
+/*Console.WriteLine($"Sphere surface area: {sphereSurfaceArea:0.00}");
+Console.WriteLine($"Sphere volume2: {sphereVolume:0.00}");*/
