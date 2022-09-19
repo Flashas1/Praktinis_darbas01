bdfc9b6de18de2efb4ba95c79be4434f6eb66a78
Dovis <dovydas.cizauskas@gmail.com>
2022-09-19 17:56:22 +0300

    4 dalis

--- Praktinis_darbas01/Program.cs
+++ Praktinis_darbas01/Program.cs
@@ -17,3 +17,16 @@ double sphereVolume = 4.0 / 3 * numberPI * Math.Pow(sphereRadius, 3);
 
 /*Console.WriteLine($"Sphere surface area: {sphereSurfaceArea:0.00}");
 Console.WriteLine($"Sphere volume2: {sphereVolume:0.00}");*/
+
+Console.WriteLine($"Kintamasis 1: {sphereSurfaceArea}");
+Console.WriteLine($"Kintamasis 2: {sphereVolume}");
+Console.WriteLine($"Kintamasis 3: {izambine}");
+
+double kintamujuSuma = 0;
+double kintamujuVidurkis;
+
+kintamujuSuma = izambine + sphereSurfaceArea + sphereVolume;
+kintamujuVidurkis = (izambine + sphereSurfaceArea + sphereVolume) / 3;
+
+Console.WriteLine($"Suma: {kintamujuSuma}");
+Console.WriteLine($"Vidurkis: {kintamujuVidurkis}");
